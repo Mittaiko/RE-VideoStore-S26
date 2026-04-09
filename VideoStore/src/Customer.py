6e395a01522eec3586f669d541ffcd91f6c06ce9
Angela Le <155772535+Mittaiko@users.noreply.github.com>
2026-04-09 17:17:58 -0500

    Refactor frequent renter points calculation in statement

--- VideoStore/src/Customer.py
+++ VideoStore/src/Customer.py
@@ -21,23 +21,11 @@ class Customer:
         result = f"Rental Record for {self.get_name()}\n"
         total_amount = 0.0
         frequent_renter_points = 0
-
         for rental in self.rentals:
-            frequent_renter_points += 1
-
-            # New releases rented for more than one day gives a bonus rental point
-            if rental.get_movie().get_price_code() == Movie.NEW_RELEASE and rental.get_days_rented() > 1:
-                frequent_renter_points += 1
-
-            # Title of rental
+            frequent_renter_points += rental.get_frequent_renter_points()
+            # Title and charge for this rental
             result += f"\t{rental.get_movie().get_title()}\t" + f"{rental.get_charge():.2f}".rstrip('0').rstrip('.') + "\n"
             total_amount += rental.get_charge()
-
-        # Total amount owed
         result += f"Amount owed is: {total_amount:.2f}".rstrip('0').rstrip('.') + "\n"
-
-        # Frequent renter points earned
         result += f"You earned: {frequent_renter_points} frequent renter points\n"
-
         return result
-
